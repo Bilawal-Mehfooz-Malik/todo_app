7d3d8ac37a3f30f3dc4885370784871dc12c3a34
Bilawal-Mehfooz-Malik <bilawalmehfoozflutter@gmail.com>
2024-04-16 19:31:12 +0500

    code cleanup

--- lib/src/features/add_tasks/presentation/add_tasks_screen.dart
+++ lib/src/features/add_tasks/presentation/add_tasks_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/src/features/add_tasks/presentation/add_task_content.dart';
+
+// local imports
+import 'package:todo_app/src/constants/app_sizes.dart';
+import 'package:todo_app/src/common/alert_dialogues.dart';
+import 'package:todo_app/src/common/custom_elevated_button.dart';
+import 'package:todo_app/src/features/add_tasks/presentation/priority_radio_section.dart';
+import 'package:todo_app/src/features/add_tasks/presentation/add_task_textfield_section.dart';
 
 class AddTaskScreen extends StatelessWidget {
   const AddTaskScreen({super.key});
@@ -8,7 +14,32 @@ class AddTaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Add Todo')),
-      body: const AddTaskContent(),
+      body: Padding(
+        padding: const EdgeInsets.all(Sizes.p8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Task title and description fields
+            const AddTaskTextFieldsSection(),
+
+            // Priority Section ([Priority.urgent], [Priority.normal])
+            const PriorityRadioSection(),
+            const Spacer(),
+
+            // Save Todo Button
+            CustomElevatedButton(
+              text: 'Save',
+              onPressed: () => _saveTodo(context),
+            ),
+            gapH4,
+          ],
+        ),
+      ),
     );
+  }
+
+  void _saveTodo(BuildContext context) {
+    // TODO: Implement save method
+    showNotImplementedAlertDialog(context: context);
   }
 }
