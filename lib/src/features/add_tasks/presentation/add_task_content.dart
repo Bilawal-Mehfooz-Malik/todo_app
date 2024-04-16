b62957dc34246715d3c7bdf195040da732736fad
Bilawal-Mehfooz-Malik <bilawalmehfoozflutter@gmail.com>
2024-04-16 19:23:14 +0500

    added priority radio buttons and save button

--- lib/src/features/add_tasks/presentation/add_task_content.dart
+++ lib/src/features/add_tasks/presentation/add_task_content.dart
@@ -2,62 +2,38 @@ import 'package:flutter/material.dart';
 
 // local imports
 import 'package:todo_app/src/constants/app_sizes.dart';
-import 'package:todo_app/src/common/custom_textformfield.dart';
-import 'package:todo_app/src/localization/string_hardcoded.dart';
+import 'package:todo_app/src/common/alert_dialogues.dart';
+import 'package:todo_app/src/common/custom_elevated_button.dart';
+import 'package:todo_app/src/features/add_tasks/presentation/priority_radio_section.dart';
+import 'package:todo_app/src/features/add_tasks/presentation/add_task_textfield_section.dart';
 
-class AddTaskContent extends StatefulWidget {
+class AddTaskContent extends StatelessWidget {
   const AddTaskContent({super.key});
 
   @override
-  State<AddTaskContent> createState() => _AddTaskContentState();
-}
-
-class _AddTaskContentState extends State<AddTaskContent> {
-  final _titleController = TextEditingController();
-  final _descriptionController = TextEditingController();
-
-  @override
-  void dispose() {
-    _titleController.dispose();
-    _descriptionController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final style = Theme.of(context);
+    // final style = Theme.of(context);
     return Form(
       child: Padding(
         padding: const EdgeInsets.all(Sizes.p8),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            CustomTextFormField(
-              label: 'Title'.hardcoded,
-              controller: _titleController,
-            ),
-            gapH8,
-
-            CustomTextFormField(
-              label: 'Description'.hardcoded,
-              controller: _descriptionController,
-            ),
-            gapH16,
+            const AddTaskTextFieldsSection(),
 
             // Priority Section
-            Text(
-              'Choose the priority of your work',
-              style: style.textTheme.titleMedium,
+            const PriorityRadioSection(),
+            const Spacer(),
+
+            // Save Todo Button
+            CustomElevatedButton(
+              text: 'Save',
+              onPressed: () {
+                // TODO: Implement save method
+                showNotImplementedAlertDialog(context: context);
+              },
             ),
-            // Row(
-            //   children: [
-            //     RadioListTile(
-            //       value: value,
-            //       groupValue: groupValue,
-            //       onChanged: onChanged,
-            //     ),
-            //   ],
-            // )
+            gapH4,
           ],
         ),
       ),
---
+++ lib/src/features/add_tasks/presentation/add_task_textfield_section.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/material.dart';
+import 'package:todo_app/src/constants/app_sizes.dart';
+import 'package:todo_app/src/common/custom_textformfield.dart';
+import 'package:todo_app/src/localization/string_hardcoded.dart';
+
+class AddTaskTextFieldsSection extends StatefulWidget {
+  const AddTaskTextFieldsSection({super.key});
+
+  @override
+  State<AddTaskTextFieldsSection> createState() =>
+      _AddTaskTextFieldsSectionState();
+}
+
+class _AddTaskTextFieldsSectionState extends State<AddTaskTextFieldsSection> {
+  final _titleController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomTextFormField(
+          label: 'Title'.hardcoded,
+          controller: _titleController,
+        ),
+        gapH8,
+        CustomTextFormField(
+          label: 'Description'.hardcoded,
+          controller: _descriptionController,
+        ),
+        gapH16,
+      ],
+    );
+  }
+}
